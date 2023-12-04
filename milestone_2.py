89123f157db60991fb343d60ecc27d91e16e8f0b
FansuJ107 <fjanneh@hotmail.co.uk>
2023-12-04 00:34:18 +0000

    Added code for milestone_2 choosing a letter

--- milestone_2.py
+++ milestone_2.py
@@ -1,9 +1,14 @@
 import random
-word_list = ["grape", "lychee", "watermelon", "mango", "apple"]
-guess = input("Enter a single letter: “) [0] 
-if len(guess) == 1 and user_input.isalpha():
+
+word_list = ['Grape', 'Lychee', 'Watermelon', 'Mango', 'Apple']
+print(word_list)
+word = random.choice(word_list)
+print(word)
+guess = input("Enter a single letter: ")
+               
+if len(guess) == 1 and guess.isalpha():
     # Do something
     print("Good guess")
 else:
     # Do something else
-    print("Oops! That is not a valid input.")
+    print('Oops! That is not a valid input.')
